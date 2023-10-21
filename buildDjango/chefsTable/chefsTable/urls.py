d162d3b869192503a8b4921bb08b60a9a38febdd
Rahul Bansod <rahulbansod519@gmail.com>
2023-10-21 05:36:51 +0000

    created new app

--- buildDjango/chefsTable/chefsTable/urls.py
+++ buildDjango/chefsTable/chefsTable/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('say_hello/', views.say_hello),
 ]
